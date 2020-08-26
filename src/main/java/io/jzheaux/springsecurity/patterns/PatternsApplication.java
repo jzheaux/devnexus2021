a119cf565bd506a86c32e140d3c952fc2dd7070b
Eleftheria Stein <eleftheria.kousathana@gmail.com>
2020-08-26 18:20:58 +0200

    Make application into a resource server

--- src/main/java/io/jzheaux/springsecurity/patterns/PatternsApplication.java
+++ src/main/java/io/jzheaux/springsecurity/patterns/PatternsApplication.java
@@ -2,9 +2,7 @@ package io.jzheaux.springsecurity.patterns;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.security.core.Authentication;
 import org.springframework.security.core.annotation.CurrentSecurityContext;
-import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
